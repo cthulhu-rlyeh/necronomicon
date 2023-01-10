a5148dab4a1c4524a123af859970199fcf238c42
Cthulhu <cfrlyeh@gmail.com>
2023-01-10 15:55:00 -0600

    feat: basic server connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use lazy_static::lazy_static;
+
+lazy_static!{
+    static ref ARGS: Vec<String> = std::env::args().collect();
+    static ref SERVER: String = ARGS[1].clone();
+    static ref PORT: String = ARGS[2].clone();
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rcl = redis::Client::open(format!("redis://{}:{}", *SERVER, *PORT))?;
+    let mut rc = rcl.get_connection()?;
+    println!("We say ping, server says: {}", redis::cmd("ping").query::<String>(&mut rc)?);
+    Ok(())
 }
