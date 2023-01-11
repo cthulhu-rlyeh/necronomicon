88eb485c146c94a108f21d1bf64cd1f2c0de0922
Cthulhu <117217464+cthulhu-rlyeh@users.noreply.github.com>
2023-01-10 22:56:11 -0600

    fix: check server response

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ lazy_static!{
     static ref PORT: String = ARGS[2].clone();
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rcl = redis::Client::open(format!("redis://{}:{}", *SERVER, *PORT))?;
     let mut rc = rcl.get_connection()?;
-    println!("We say ping, server says: {}", redis::cmd("ping").query::<String>(&mut rc)?);
+    let ping = redis::cmd("ping").query::<String>(&mut rc)?;
+    println!("we say ping, server says: {}", redis::cmd("ping").query::<String>(&mut rc)?);
+    if ping != "PONG" { panic!("server did not pong..."); }
+    
     Ok(())
 }
