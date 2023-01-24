599c05d335708bfe1dd57f931823cc87fdb434c3
Cthulhu <cfrlyeh@gmail.com>
2023-01-24 14:27:45 -0600

    fix: rework away from using kad
    will just use floodsub instead with local caches

--- src/main.rs
+++ src/main.rs
@@ -1,214 +1,109 @@
-use std::error::Error;
-use libp2p::futures::StreamExt;
-use serde_derive::{Serialize, Deserialize};
-use tokio::time::{sleep, Duration};
-use libp2p::kad::record::store::MemoryStore;
-use libp2p::kad::{
-    record::Key, AddProviderOk, Kademlia, KademliaEvent, PeerRecord,
-    PutRecordOk, QueryResult, Quorum, Record,
+use std::{
+    error::Error,
+    collections::HashMap,
 };
+use tokio::io::{self, AsyncBufReadExt};
 use libp2p::{
-    tokio_development_transport, identity, mdns, PeerId, Swarm,
+    identity, mdns, mplex, noise, tcp, Multiaddr, PeerId, Transport,
+    core::upgrade,
+    futures::StreamExt,
+    floodsub::{self, Floodsub, FloodsubEvent},
     swarm::{NetworkBehaviour, SwarmEvent},
 };
-use libp2p_kad::{GetProvidersOk, GetRecordOk};
-use rand::prelude::*;
+lazy_static::lazy_static!(
 
-lazy_static::lazy_static!{
-    static ref ARGS: Vec<String> = std::env::args().collect();
-    static ref TEMPO: u64 = 37;
-    static ref SHORT_TICKS: i32 = 10;
-    static ref LONG_TICKS: i32 = 100;
-}
-
-#[derive(Serialize,Deserialize)]
-struct DeviceData {
-    mac: String,
-    ip: String,
-    name: String,
-    os: String,
-}
-
+);
 #[derive(NetworkBehaviour)]
-#[behaviour(out_event = "NetworkEvent")]
-struct Network {
-    kademlia: Kademlia<MemoryStore>,
-    mdns: mdns::async_io::Behaviour,
+#[behaviour(out_event = "MyBehaviourEvent")]
+struct MyBehaviour {
+    floodsub: Floodsub,
+    mdns: mdns::tokio::Behaviour,
 }
 #[allow(clippy::large_enum_variant)]
-enum NetworkEvent {
-    Kademlia(KademliaEvent),
+enum MyBehaviourEvent {
+    Floodsub(FloodsubEvent),
     Mdns(mdns::Event),
 }
-impl From<KademliaEvent> for NetworkEvent {
-    fn from(event: KademliaEvent) -> Self {
-        NetworkEvent::Kademlia(event)
+impl From<FloodsubEvent> for MyBehaviourEvent {
+    fn from(event: FloodsubEvent) -> Self {
+        MyBehaviourEvent::Floodsub(event)
     }
 }
-impl From<mdns::Event> for NetworkEvent {
+impl From<mdns::Event> for MyBehaviourEvent {
     fn from(event: mdns::Event) -> Self {
-        NetworkEvent::Mdns(event)
+        MyBehaviourEvent::Mdns(event)
     }
 }
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error+Send+Sync>> {
-    let mut console = false;
-    if ARGS.len() > 0 {
-        if ARGS[0] == "console" {
-            console = true;
-        }
-    }
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let keys = identity::Keypair::generate_ed25519();
+    let id = PeerId::from(keys.public());
+    let transport = tcp::tokio::Transport::new(tcp::Config::default().nodelay(true))
+        .upgrade(upgrade::Version::V1)
+        .authenticate(noise::NoiseAuthenticated::xx(&keys)?)
+        .multiplex(mplex::MplexConfig::new())
+    .boxed();
 
-    let mut rng = rand::rngs::OsRng;
+    let topic = floodsub::Topic::new("chat");
 
-    tokio::spawn(swarm_handler());
-    
-    let mut short_ticker = *SHORT_TICKS;
-    let mut long_ticker = *LONG_TICKS;
-    loop {
-        // todo: add fastest actions here
-        // if necromancer
-            // todo: if too many necromancers are in the swarm, demote
-        // else
-            // todo: if no necromancer is found in the swarm, promote
-        // end if
-
-        if rng.gen::<bool>() {
-            short_ticker += 1;
-            if short_ticker > *SHORT_TICKS {
-                // todo: add slower actions here
-                // todo: collect system data and store in swarm
-                // todo: check swarm for zombie tasks and run accordingly
-                // if necromancer
-                    // todo: sync swarm tasks with graveyard
-                    // todo: upload current swarm data to graveyard
-                    // todo: if graveyard unreachable, demote
-                // end if
-
-                println!("-> beep");
-                short_ticker = 0;
-            }
-        }
-
-        if rng.gen::<bool>() {
-            long_ticker += 1;
-            if long_ticker > *LONG_TICKS {
-                // todo: add slowest actions here
-                // if necromancer
-                    // todo: sweep for lonely systems without a zombie
-                    // todo: spawn zombies on lonely systems
-                // end if
-                println!("---> boop");
-                long_ticker = 0;
-            }
-        }
-
-        sleep(Duration::from_secs(*TEMPO)).await;
-    }
-}
-
-async fn swarm_handler() -> Result<(), Box<dyn Error+Send+Sync>> {
-    let local_key = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(local_key.public());
-    let transport = tokio_development_transport(local_key)?;
-
-    let mut swarm = {
-        let store = MemoryStore::new(local_peer_id);
-        let kademlia = Kademlia::new(local_peer_id, store);
-        let mdns = mdns::async_io::Behaviour::new(mdns::Config::default())?;
-        let network = Network { kademlia, mdns };
-        Swarm::with_tokio_executor(transport, network, local_peer_id)
+    let mdns_behaviour = mdns::Behaviour::new(mdns::Config::default())?;
+    let mut behaviour = MyBehaviour {
+        floodsub: Floodsub::new(id),
+        mdns: mdns_behaviour,
     };
 
-    swarm.listen_on("/ipv4/0.0.0.0/tcp/0".parse()?)?;
+    behaviour.floodsub.subscribe(topic.clone());
+
+    let mut swarm = libp2p::swarm::Swarm::with_tokio_executor(transport, behaviour, id);
+
+    if let Some(to_dial) = std::env::args().nth(1) {
+        let addr: Multiaddr = to_dial.parse()?;
+        swarm.dial(addr)?;
+        println!("dialed {to_dial:?}");
+    }
+
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    let cache: HashMap<u64, String> = HashMap::new();
     loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => {
-                println!("listening on {address:?}");
-            },
-            SwarmEvent::Behaviour(NetworkEvent::Mdns(mdns::Event::Discovered(list))) => {
-                for (peer_id, multiaddr) in list {
-                    swarm.behaviour_mut().kademlia.add_address(&peer_id, multiaddr);
-                }
+        tokio::select!{
+            line = stdin.next_line() => {
+                let line = line?.expect("stdin closed");
+                swarm.behaviour_mut().floodsub.publish(topic.clone(), line.as_bytes());
             }
-            SwarmEvent::Behaviour(NetworkEvent::Kademlia(KademliaEvent::OutboundQueryProgressed { result, ..})) => {
-                match result {
-                    QueryResult::GetProviders(Ok(GetProvidersOk::FoundProviders { key, providers, .. })) => {
-                        for peer in providers {
-                            println!(
-                                "peer {peer:?} provides key {:?}",
-                                std::str::from_utf8(key.as_ref()).unwrap()
+            event = swarm.select_next_some() => {
+                match event {
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        println!("listening to {address:?}");
+                    }
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Floodsub(FloodsubEvent::Message(message))) => {
+                        println!(
+                                "received from {:?}: {:?}",
+                                message.source,
+                                String::from_utf8_lossy(&message.data),
                             );
+                    }
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(event)) => {
+                        match event {
+                            mdns::Event::Discovered(list) => {
+                                for (peer, _) in list {
+                                    swarm.behaviour_mut().floodsub.add_node_to_partial_view(peer);
+                                }
+                            }
+                            mdns::Event::Expired(list) => {
+                                for (peer, _) in list {
+                                    if !swarm.behaviour().mdns.has_node(&peer) {
+                                        swarm.behaviour_mut().floodsub.remove_node_from_partial_view(&peer);
+                                    }
+                                }
+                            }
                         }
-                    }
-                    QueryResult::GetProviders(Err(err)) => {
-                        eprintln!("failed to get providers: {err:?}");
-                    }
-                    QueryResult::GetRecord(Ok(
-                        GetRecordOk::FoundRecord(PeerRecord {
-                            record: Record { key, value, .. },
-                            ..
-                        })
-                    )) => {
-                        println!(
-                            "got record {:?} {:?}",
-                            std::str::from_utf8(key.as_ref()).unwrap(),
-                            std::str::from_utf8(&value).unwrap(),
-                        );
-                    }
-                    QueryResult::GetRecord(Ok(_)) => {}
-                    QueryResult::GetRecord(Err(err)) => {
-                        eprintln!("failed to get record: {err:?}");
-                    }
-                    QueryResult::PutRecord(Ok(PutRecordOk { key })) => {
-                        println!(
-                            "successfully put record {:?}",
-                            std::str::from_utf8(key.as_ref()).unwrap()
-                        );
-                    }
-                    QueryResult::PutRecord(Err(err)) => {
-                        eprintln!("failed to put record: {err:?}");
-                    }
-                    QueryResult::StartProviding(Ok(AddProviderOk { key })) => {
-                        println!(
-                            "successfully put provider record {:?}",
-                            std::str::from_utf8(key.as_ref()).unwrap()
-                        );
-                    }
-                    QueryResult::StartProviding(Err(err)) => {
-                        eprintln!("failed to put provider record: {err:?}");
                     }
                     _ => {}
                 }
             }
-            _ => {}
         }
-    }
-}
-
-fn _collector() -> String {
-    let os = os_info::get();
-    let data = DeviceData {
-        mac: match mac_address::get_mac_address() {
-            Err(_) => "".to_owned(),
-            Ok(ok) => match ok {
-                None => "".to_owned(),
-                Some(ok) => ok.to_string(),
-            },
-        },
-        ip: match local_ip_address::local_ip() {
-            Err(_) => "".to_owned(),
-            Ok(ok) => ok.to_string(),
-        },
-        name: match hostname::get() {
-            Err(_) => "".to_owned(),
-            Ok(ok) => ok.to_string_lossy().to_string(),
-        },
-        os: [os.os_type().to_string(), os.version().to_string(), os.bitness().to_string()].join(" "),
-    };
-    match serde_json::to_string(&data) {
-        Err(_) => "".to_owned(),
-        Ok(ok) => ok,
     }
 }
